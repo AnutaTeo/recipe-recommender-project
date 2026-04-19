866b741abc0ae660fe131eac228f2b251ebf5bad
AnutaTeo <amam.teodorescu@gmail.com>
2026-04-19 18:05:52 +0300

    initial project setup

---
+++ recipe-recommender/src/test/java/com/example/recipe_recommender/RecipeRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe_recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
